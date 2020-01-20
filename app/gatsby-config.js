84895570fd1756098793843c350a6c52ff40707b
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2020-01-20 12:25:10 -0800

    Configure purgecss gatsby plugin

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         short_name: "BSK Portfolio",
         start_url: "/",
         display: "minimal-ui",
-        icon: "src/images/icons/brand-icon.svg", // This path is relative to the root of the site.
+        icon: "src/images/icons/brand-icon.svg",
       },
     },
     {
@@ -33,6 +33,34 @@ module.exports = {
         postCssPlugins: [tailwindcss("./tailwind.config.js")],
       },
     },
+
+    /**
+     * ---------------------------------------------------------------------------
+     * PurgeCSS          https://www.gatsbyjs.org/packages/gatsby-plugin-purgecss/
+     * ---------------------------------------------------------------------------
+     *
+     * Remove unused css from css/sass/less/stylus files and modules in your
+     * Gatsby project using purgecss.
+     *
+     * NOTE: Add the plugin AFTER other css/postcss plugins
+     *
+     * Available options:
+     * {
+     *   printRejected: true, // Print removed selectors and processed file names
+     *   develop: true, // Enable while using `gatsby develop`
+     *   tailwind: true, // Enable tailwindcss support
+     *   whitelist: ['whitelist'], // Prevent removal this list of selectors
+     *   ignore: ['/ignored.css'], // Ignore purge of this list of files/folders
+     *   purgeOnly : ['components', '/main.css'], // Purge specific files/folders
+     * }
+     */
+
     "gatsby-plugin-tailwindcss",
+    {
+      resolve: "gatsby-plugin-purgecss",
+      options: {
+        tailwind: true,
+      },
+    },
   ],
 };
