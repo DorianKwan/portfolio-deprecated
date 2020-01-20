911f48c4eaeb66a68a299bed8311c6eac82a8e91
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2020-01-20 13:06:46 -0800

    Edit storybook config file to use double quotes

--- app/.storybook/config.js
+++ app/.storybook/config.js
@@ -1,7 +1,7 @@
-import { configure, addDecorator } from '@storybook/react';
-import { withA11y } from '@storybook/addon-a11y';
+import { configure, addDecorator } from "@storybook/react";
+import { withA11y } from "@storybook/addon-a11y";
 
 addDecorator(withA11y);
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src/components/stories', true, /\.stories\.js$/), module);
+configure(require.context("../src/components/stories", true, /\.stories\.js$/), module);
