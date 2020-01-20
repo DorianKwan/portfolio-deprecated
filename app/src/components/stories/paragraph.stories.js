9816fd33caf96e5a230aa6c56fd15d985e24bbb5
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2020-01-19 16:12:50 -0800

    Create paragraph component story

---
+++ app/src/components/stories/paragraph.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Paragraph from "../paragraph";
+
+export default { title: "Paragraph" };
+
+export const defaultOptions = () => {
+  return (
+    <Paragraph content="This is a paragraph component with default options." />
+  );
+};
