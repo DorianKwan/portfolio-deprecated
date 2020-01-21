07cbdf2b863e6e51521e34c5f985d1e9a0384da0
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2020-01-20 20:17:32 -0800

    Add cssReset, tailwind css and Knobs to storybook config

--- app/.storybook/config.js
+++ app/.storybook/config.js
@@ -1,7 +1,15 @@
 import { configure, addDecorator } from "@storybook/react";
 import { withA11y } from "@storybook/addon-a11y";
+import { withKnobs } from "@storybook/addon-knobs";
+
+// Add cssReset by default
+import "../src/styles/cssReset.scss";
+
+// Add tailwind css utility classes by default
+import "../src/styles/tailwind.css";
 
 addDecorator(withA11y);
+addDecorator(withKnobs);
 
 // automatically import all files ending in *.stories.js
 configure(require.context("../src/components/stories", true, /\.stories\.js$/), module);
