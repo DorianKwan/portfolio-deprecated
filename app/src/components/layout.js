103fe05e55dedca90fee3e575c31857efab93484
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2020-01-24 16:27:00 -0800

    Add react-app-polyfill/ie11 to layout and stortybook config

--- app/src/components/layout.js
+++ app/src/components/layout.js
@@ -1,3 +1,16 @@
+/**
+ * https://github.com/facebook/create-react-app/blob/master/packages/react-app-polyfill/README.md#react-app-polyfill
+ *
+ * This is add support of the following to IE 11
+ *
+ * - Promise (for async / await support)
+ * - window.fetch (a Promise-based way to make web requests in the browser)
+ * - Object.assign (a helper required for Object Spread, i.e. { ...a, ...b })
+ * - Symbol (a built-in object used by for...of syntax and friends)
+ * - Array.from (a built-in static method used by array spread, i.e. [...arr])
+ */
+import "react-app-polyfill/ie11";
+
 import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
