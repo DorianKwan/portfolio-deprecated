e70af7011bb78a00ab570678142c24b3e7c9d52f
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2020-01-25 17:46:06 -0800

    Add navigation to header component

--- app/src/components/header.js
+++ app/src/components/header.js
@@ -1,13 +1,79 @@
-import { Link } from "gatsby";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import React from "react";
+import { useScrollPosition } from "@n8tb1t/use-scroll-position";
+import { ReactSVG } from "react-svg";
+import { Link } from "gatsby";
 
-const Header = ({ siteTitle }) => {
+import "./header.scss";
+
+import brandLogoSRC from "../images/icons/brand-logo.svg";
+
+const getRandomNumber = (min = 1, max = 9999) => {
+  return Math.random() * (max - min) + min;
+};
+
+const buildNavlinks = (navigationData) => {
+  const navLinks = [];
+  const linkClasses = "px-8 py-2 text-inherit uppercase font-medium";
+
+  navigationData.forEach((link) => {
+    const { title, href } = link;
+    const linkKey = `${title}-${getRandomNumber()}`;
+    navLinks.push(
+      <li className="pl-28"><Link to={href} key={linkKey} className={linkClasses}>{title}</Link></li>
+    );
+  });
+
+  return navLinks;
+};
+
+const Header = ({ siteTitle, navigationData }) => {
+  const [headerStyle, setHeaderStyle] = useState({
+    transition: "all 500ms linear",
+    color: "white",
+  });
+
+  useScrollPosition(
+    (position) => {
+      const { currPos } = position;
+      const isTransparent = currPos.y > -1;
+
+      const shouldBeStyle = {
+        transition: "all 500ms linear",
+        color: isTransparent ? "white" : "#544C4A", // harbor
+        "background-color": isTransparent ? "transparent" : "white",
+        "box-shadow": isTransparent ? "none" : "0px 0px 15px -5px rgba(0,0,0,0.25)",
+      };
+
+      if (JSON.stringify(shouldBeStyle) === JSON.stringify(headerStyle)) return;
+
+      setHeaderStyle(shouldBeStyle);
+    }
+  );
+
+  const links = buildNavlinks(navigationData);
+
   return (
-    <header>
-      <Link to="/">
-        {siteTitle}
+    <header
+      className="navbar w-full max-w-1600 h-80 flex flex-row justify-between items-center fixed -top-1"
+      style={{ ...headerStyle }}
+    >
+      <Link to="/" className="text-inherit flex flex-row">
+        <span className="sr-only">Go to home:</span>
+        <ReactSVG
+          src={brandLogoSRC}
+          className="flex items-center mb-4 pr-8"
+          beforeInjection={(svg) => {
+            svg.setAttribute("style", "width: 30px;");
+          }}
+        />
+        <h1 className="font-normal">{siteTitle}</h1>
       </Link>
+      <nav>
+        <ul className="flex flex-row">
+          {links}
+        </ul>
+      </nav>
     </header>
   );
 };
@@ -16,8 +82,10 @@ export default Header;
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
+  navigationData: PropTypes.arrayOf(PropTypes.object),
 };
 
 Header.defaultProps = {
   siteTitle: "",
+  navigationData: [],
 };
